89cb6e380d5fae55c166972206ec25aac5875118
Pierre Chifflier <chifflier@wzdftpd.net>
2020-03-18 15:37:40 +0100

    Benchmarks: assert result is a success

--- benches/bench.rs
+++ benches/bench.rs
@@ -8,13 +8,15 @@ static IGCA_DER: &'static [u8] = include_bytes!("../assets/IGC_A.der");
 #[bench]
 fn bench_parse_x509_rs(b: &mut Bencher) {
     b.iter(|| {
-        let _ = bench_x509::decode_x509_rs(IGCA_DER);
+        let res = bench_x509::decode_x509_rs(IGCA_DER);
+        assert!(res.is_some());
     });
 }
 
 #[bench]
 fn bench_parse_x509_c(b: &mut Bencher) {
     b.iter(|| {
-        let _ = bench_x509::decode_x509_c(IGCA_DER);
+        let res = bench_x509::decode_x509_c(IGCA_DER);
+        assert!(res != ::std::ptr::null_mut());
     });
 }
